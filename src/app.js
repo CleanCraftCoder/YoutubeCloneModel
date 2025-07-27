07f7789f668ef8344e654fc07200c79740a3e9c1
Hasan Nawaz <hncse@gmail.com>
2025-07-27 18:09:54 +0530

    created /users that is first route for user then it goes to user.routes.js for further routes like /register in users. we made controller for user to register(post). Here in registerUser all logic for register the user

--- src/app.js
+++ src/app.js
@@ -9,4 +9,13 @@ app.use(cookieParser());
 app.use(express.json({limit: '16kb'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public')); // Serve static files from the 'public' directory
+
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+
+//routes declaration
+app.use("/api/v1/users",userRouter)
+//https://localhost:3000/api/v1/users/jobhi ho uske baad run karenge
 export {app}
